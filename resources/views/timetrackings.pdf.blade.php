8d03fcc31bea887506c8ebc5c168e86be69c8d88
nayere <rebecca.nayere@gmail.com>
2024-03-20 11:40:55 +0300

    add timetrackingspdf blade in resources

---
+++ resources/views/timetrackings.pdf.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Time Tracking Entries</h1>
+        
+        <!-- Display time tracking entries here -->
+        <div class="mt-6">
+            <a href="{{ route('timetrackings.export.excel') }}" class="btn btn-success">Export to Excel</a>
+            <a href="{{ route('timetrackings.export.pdf') }}" class="btn btn-primary">Export to PDF</a>
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>From Date</th>
+                    <th>To Date</th>
+                    <th>Code</th>
+                    <th>Department</th>
+                    <th>ID Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($timetrackings as $timetracking)
+                    <tr>
+                        <td>{{ $timetracking->employee->FromDate }}</td>
+                        <td>{{ $timetracking->employee->ToDate }}</td>
+                        <td>{{ $timetracking->Code }}</td>
+                        <td>{{ $timetracking->Department }}</td>
+                        <td>{{ $timetracking->IDNumber }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
